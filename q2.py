c35a303f4e83848a5980e5d42e186ef8556f3005
Rodrigo Moreno <luiromormor@gmail.com>
2023-11-18 22:23:11 +0100

    Style and clarity corrections on q2.
    Fixed some small style 'mistakes' that slow down code a bit.
    Added functionality to save figures instantly instead of manually.

--- q2.py
+++ q2.py
@@ -41,34 +41,35 @@ def dispatch(mtype, hvalue):
         raise TypeError(f'Model of type {mtype} is not supported.')
 
 
-def variance_bias_computation_kfold(model_type, hvalue, X_train, y_train, X_test, y_test, random_seed, n_splits):
+def variance_bias_computation_kfold(model_type, hvalue, X_train, y_train,
+                                    X_test, y_test, random_seed, n_splits):
     """
-        A function that computes the expected error, variance and bias values for a certain model.
+    A function that computes the expected error, variance and bias values
+    for a certain model.
 
-        Input:
-        - model_type: str specifying what type of model is being studied
-        - hvalue: value of the hyper-parameter integrated in the model
-        - X_train: Training set features
-        - y_train: Training set labels
-        - X_test: Test set features
-        - y_test: Testing set labels
-        - random_seed
-        - n_splits: Number of splits of kfold
+    Input:
+    - model_type: str specifying what type of model is being studied
+    - hvalue: value of the hyper-parameter integrated in the model
+    - X_train: Training set features
+    - y_train: Training set labels
+    - X_test: Test set features
+    - y_test: Testing set labels
+    - random_seed
+    - n_splits: Number of splits of kfold
 
-        Output:
-        - avg_expected_loss: average expected loss for the given model
-        - avg_bias: average bias for the given model
-        - avg_variance: average variance for the given model
-        """
+    Output:
+    - avg_expected_loss: average expected loss for the given model
+    - avg_bias: average bias for the given model
+    - avg_variance: average variance for the given model
+    """
 
     np.random.seed(random_seed)
     kfold = KFold(n_splits=n_splits, shuffle=True, random_state=random_seed)
 
-    errors = []
+    errors = np.zeros(n_splits)
     all_predictions = np.zeros((n_splits, len(y_test)))
-    fold_idx = 0
 
-    for train_index, val_index in kfold.split(X_train):
+    for fold_idx, (train_index, val_index) in enumerate(kfold.split(X_train)):
         X_train_fold, X_val_fold = X_train[train_index], X_train[val_index]
         y_train_fold, y_val_fold = y_train[train_index], y_train[val_index]
 
@@ -77,9 +78,12 @@ def variance_bias_computation_kfold(model_type, hvalue, X_train, y_train, X_test
         trained_model = model.fit(X_train_fold, y_train_fold)
         predictions = trained_model.predict(X_test)
 
-        all_predictions[fold_idx] = predictions
-        errors.append(mean_squared_error(y_test, predictions))
-        fold_idx += 1
+        all_predictions[fold_idx, :] = predictions
+        errors[fold_idx] = mean_squared_error(y_test, predictions)
+        #print(predictions[:5])
+        #print(y_test[:5])
+        #print(y_test[:5] - predictions[:5])
+        #print(errors[fold_idx])
 
     # Calculate average expected loss, bias, and variance
     avg_expected_loss = np.mean(errors)
@@ -90,38 +94,41 @@ def variance_bias_computation_kfold(model_type, hvalue, X_train, y_train, X_test
     return avg_expected_loss, avg_bias, avg_var
 
 
-def models_assessment(model_type, hvalue, X_train, y_train, X_test, y_test):
+def models_assessment(model_type, hvalues, X_train, y_train, X_test, y_test):
     """
     Parameters
     ----------
     model_type: string. Represents the type of model required.
-    hvalue: array containing hyperparameter values
+    hvalues: array containing hyperparameter values
     X_train: array containing training samples
     y_train: array containing training labels
     X_test: array containing test samples
     y_test: array containing test labels
-
     """
-    bias_Class, var_Class, error_Class, = [], [], []
+    #bias_Class, var_Class, error_Class, = [], [], []
 
-    for k in range(1, len(hvalue)):
-
-        avg_expected_loss, avg_bias, avg_var = variance_bias_computation_kfold(model_type, hvalue[k], X_train, y_train,
+    bias_Class = np.zeros(len(hvalues))
+    var_Class = np.zeros(len(hvalues))
+    error_Class = np.zeros(len(hvalues))
+    for ii, kk in enumerate(hvalues):#range(len(hvalue)):
+        avg_expected_loss, avg_bias, avg_var = variance_bias_computation_kfold(model_type, kk, X_train, y_train,
                                                                        X_test ,y_test, random_seed=123, n_splits=10)
-        bias_Class.append(avg_bias)
-        var_Class.append(avg_var)
-        error_Class.append(avg_expected_loss)
-        print(f"Average expected loss with model {model_type} and hyperparameter {hvalue[k]}: {avg_expected_loss}")
-        print(f"Average bias with model {model_type} and hyperparameter {hvalue[k]}: {avg_bias}")
-        print(f"Average variance with model {model_type} and hyperparameter {hvalue[k]}: {avg_var}")
+        bias_Class[ii] = avg_bias
+        var_Class[ii] = avg_var
+        error_Class[ii] = avg_expected_loss
+        #print(f"Average expected loss with model {model_type} and hyperparameter {hvalue[k]}: {avg_expected_loss}")
+        #print(f"Average bias with model {model_type} and hyperparameter {hvalue[k]}: {avg_bias}")
+        #print(f"Average variance with model {model_type} and hyperparameter {hvalue[k]}: {avg_var}")
 
-    plt.plot(range(1, len(hvalue)), error_Class, 'red', label='total_error', linestyle='dashed')
-    plt.plot(range(1, len(hvalue)), bias_Class, 'brown', label='bias^2')
-    plt.plot(range(1, len(hvalue)), var_Class, 'yellow', label='variance')
-    plt.xlabel(f'Algorithm Complexity: {model_type}')
-    plt.ylabel('Error')
-    plt.legend()
-    plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(hvalues, error_Class, 'red', label='total_error', linestyle='dashed')
+    ax.plot(hvalues, bias_Class, 'brown', label='bias^2')
+    ax.plot(hvalues, var_Class, 'yellow', label='variance')
+    ax.set_xlabel(f'Algorithm Complexity: {model_type}')
+    ax.set_ylabel('Error')
+    ax.legend()
+    plt.savefig(f'{model_type}_{len(y_train)}_assessment.pdf', format = 'pdf')
+    #plt.show()
     return
 
 
@@ -138,14 +145,18 @@ if __name__ == '__main__':
     model_type = 'tree'
     metaparameters = np.arange(1, 50, 2)
     models_assessment(model_type, metaparameters, X_train, y_train, X_test, y_test)
+    print(f'Finished {model_type}')
 
     # assesment of Ridge Regression
     model_type = 'reg'
-    metaparameters = np.arange(1, 50, 2)
+    metaparameters = np.arange(0, 11, 2)
+    #metaparameters = np.linspace(0, 5000, 10)
     models_assessment(model_type, metaparameters, X_train, y_train, X_test, y_test)
+    print(f'Finished {model_type}')
 
     # assesment of KNN
     model_type = 'knn'
-    metaparameters = np.arange(1, 50, 2)
+    metaparameters = np.arange(1, 11, 1)
     models_assessment(model_type, metaparameters, X_train, y_train, X_test, y_test)
+    print(f'Finished {model_type}')
 
